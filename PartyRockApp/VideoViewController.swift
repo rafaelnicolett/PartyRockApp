58f01de265e918eac26f0e19b5c27ea58e1b85bb
Rafael Nicolett <rafaelnicolett@outlook.com>
2016-11-22 23:40:54 -0200

    Commit the Design

---
+++ PartyRockApp/VideoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VideoViewController.swift
+//  PartyRockApp
+//
+//  Created by Rafael Nicoleti on 22/11/16.
+//  Copyright © 2016 Rafael Nicoleti. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
