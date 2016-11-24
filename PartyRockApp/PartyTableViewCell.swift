229d1db029c59e289be2f5346133cc26b57abed6
Rafael Nicolett <rafaelnicolett@outlook.com>
2016-11-24 13:50:15 -0200

    Finish sample with load images internet and tableview

--- PartyRockApp/PartyTableViewCell.swift
+++ PartyRockApp/PartyTableViewCell.swift
@@ -18,9 +18,22 @@ class PartyTableViewCell: UITableViewCell {
         
     }
 
-    func updateUI(partyRock: PartyRock){
+    func updateUI(partyRock: PartyRock) {
         lblNameParty.text = partyRock.videoTitle
         
-        //TODO: set image from URL and load async
+        let url = URL(string: partyRock.imageURL)!
+        
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.imgParty.image = UIImage(data: data)
+                }
+            } catch  {
+                //handle the error
+            }
+        }
+        
     }
+
 }
--- PartyRockApp/VideoViewController.swift
+++ PartyRockApp/VideoViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 
 class VideoViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    private var _partyRock: PartyRock!
+    
+    var partyRock: PartyRock {
+        get {
+            return _partyRock
+        } set {
+            _partyRock = newValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        titleLbl.text = partyRock.videoTitle
+        webView.loadHTMLString(partyRock.videoURL, baseURL: nil)
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
 
 }
